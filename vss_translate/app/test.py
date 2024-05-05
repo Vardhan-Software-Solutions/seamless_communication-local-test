35c8eb90ef5a9e56f5b638b3103609516ab2685e
Sanadiip <86464813+vsssandeep@users.noreply.github.com>
2024-05-05 09:19:45 +0530

    changes to core

---
+++ vss_translate/app/test.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+import sys
+sys.path.append('../core')
+
+import os
+import pathlib
+import getpass
+
+import gradio as gr
+import numpy as np
+import torch
+import torchaudio
+from fairseq2.assets import InProcAssetMetadataProvider, asset_store
+from huggingface_hub import snapshot_download
+from seamless_communication.inference import Translator
