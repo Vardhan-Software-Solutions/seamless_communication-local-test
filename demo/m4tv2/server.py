8a2c06de9d561ed0b0f7de5ed984173e347b35fd
Sanadiip <86464813+vsssandeep@users.noreply.github.com>
2024-05-05 12:21:19 +0530

    changes pushed to m4tv2

--- demo/m4tv2/server.py
+++ demo/m4tv2/server.py
@@ -31,4 +31,5 @@ def read_file():
         return jsonify({"status": "error", "message": str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=5000)
+    # app.run(debug=True, port=5000)
