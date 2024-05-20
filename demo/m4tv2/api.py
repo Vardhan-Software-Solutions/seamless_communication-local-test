9cbe6453907596c2e54e442a1cf08f7e2ac93e27
Sanadiip <86464813+vsssandeep@users.noreply.github.com>
2024-05-20 19:52:13 +0530

    Update api.py

--- demo/m4tv2/api.py
+++ demo/m4tv2/api.py
@@ -33,7 +33,7 @@ def upload_file():
     with open('audio.json', 'w') as audio_file:
         json.dump(audio_data, audio_file)
 
-    subprocess.run(['python', 'appv2.py'])
+    subprocess.Popen(['python', 'appv2.py'])
     return jsonify({'message': 'File uploaded successfully and main.py executed'})
 
 
