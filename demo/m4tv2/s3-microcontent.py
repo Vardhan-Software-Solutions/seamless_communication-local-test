8fcc3195597823e504f59cfb4b37cf1a4d917c8f
Sanadiip <86464813+vsssandeep@users.noreply.github.com>
2024-10-07 19:57:00 +0530

    uncommented

--- demo/m4tv2/s3-microcontent.py
+++ demo/m4tv2/s3-microcontent.py
@@ -264,11 +264,11 @@ def main():
     print(transcription)
 
     print("Segmenting the transcription using GPT-4...")
-    # segments1 = segment_transcription_with_gpt(transcription)
+    segments1 = segment_transcription_with_gpt(transcription)
 
     # segments1 = segment_transcription_with_mistral(transcription)
     # print(" -------- MISTRAL ---------- ")
-    # print(segments1)
+    print(segments1)
 
     # Step 4: Segment the transcription based on context
     print("Segmenting the transcription based on context...")
