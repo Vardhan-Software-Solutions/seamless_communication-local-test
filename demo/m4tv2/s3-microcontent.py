e3921a0bcb35afad32ee86aab24f9811b2e63fb9
Sanadiip <86464813+vsssandeep@users.noreply.github.com>
2024-10-08 23:33:07 +0530

    model changed

--- demo/m4tv2/s3-microcontent.py
+++ demo/m4tv2/s3-microcontent.py
@@ -147,7 +147,7 @@ def segment_transcription_with_gpt(transcription):
                 # "content": "Hello!"
             }
             ],
-            model="gpt-3.5-turbo",
+            model="gpt-4",
         )
 
     print("response ", response)
